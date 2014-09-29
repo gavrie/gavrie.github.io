7ae2a61bef59228a2e6ed383bd54072de947d1f9
Gavrie Philipson <gavriep@il.ibm.com>
2014-09-29 17:49:57 +0300

    Site updated at 2014-09-29 14:49:57 UTC

--- source/downloads/code/buffers/execute_wrong.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def execute(command):
-    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    retval = p.wait()
-    return retval, p.stdout.read(), p.stderr.read()
-
-retval, out, err = execute("git ls-remote")
